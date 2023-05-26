5ed44687072161aa578f94c9acbeca226d93c1f7
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-26 12:15:12 +0200

    FEAT starting structure VUE

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
+</script>
+
+<template>
+
+  <Header/>
+  <Main/>
+  <Footer/>
+
+</template>
+
+<style lang="scss">
+
+@use './style/general.scss' as *;
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import "bootstrap/dist/css/bootstrap.css";
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js";
+
