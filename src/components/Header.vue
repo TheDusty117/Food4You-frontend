71366a4570170946561c933211f54d1b4100a500
Blaser993 <corradodepinto.design@gmail.com>
2023-05-29 11:37:13 +0200

    vue-link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="container">
     <h1>SONO HEADER</h1>
+    <ul class="d-flex justify-content-between">
+      <li>
+        <router-link :to="{name:home}" class="nav-link">Ristoranti</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:register}" class="nav-link">Registrati</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:login}" class="nav-link">Login</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
