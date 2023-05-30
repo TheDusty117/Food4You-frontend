45102e8ad2dec3c94ffd188377f4fa01cc0b2242
TheDusty117 <gabrieledipassio@gmail.com>
2023-05-30 17:59:05 +0200

    FEAT categories fetching and Filter WORKING

--- src/components/Restaurants.vue
+++ src/components/Restaurants.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-
+    <!-- CREA UNA checkbox
+    LA CHECKBOX ON CHANGE -- VA A CAMBIARE categoryid
+    E RILANCIA FETCHRESTAURANT -->
+    <div v-for="category in categoriesArr">
+      <label for="">{{ category.name }}</label>
+      <input type="checkbox" @click="selectCategory(category)" name="" id="">
+    </div>
+    
     <!-- CICLARE CATEGORIE PER FARE BOTTONI RICERCA FILTRO -->
-    <RestaurantsCard v-for="restaurant in restaurants" :key="restaurant.id" :restaurant="restaurant" />
+    <RestaurantsCard v-for="restaurant in filteredRestaurants" :key="restaurant.id" :restaurant="restaurant" />
   </div>
 </template>
 
@@ -16,23 +23,55 @@ import RestaurantsCard from './RestaurantsCard.vue'
     },
     data(){
       return {
+        categoriesArr: [],
+
         restaurants: [],
-        filteredRestaurants: []
+        // categoryid: 2 //passo a category giu null,(gli dico se non passa category dammi tutti i restaurants)
+        selectedCategories: []
 
       }
     },
-    methods: {
-      fetchCategories(){
-        
-      },
-      computed: {
-        filterRestoruants(){
-          return this.restaurants.filter()
+    computed: {
+        filteredRestaurants(){
+        console.log(this.selectedCategories)
+
+          if(this.selectedCategories.length>0){
+            return this.restaurants.filter(restaurant=>restaurant.categories.some(cat=>this.selectedCategories.includes(cat.id)))
+          }
+          return this.restaurants
         }
       },
+    methods: {
+      swapCat(cat){
+        this.categoryid = cat
+        this.fetchRestaurants()
+      },
+
+      selectCategory(cat){
+        const isselected=this.selectedCategories.some(category=>category===cat.id)
+        if(isselected){
+          this.selectedCategories=this.selectedCategories.filter(category=>category!=cat.id)
+        } else {
+          this.selectedCategories.push(cat.id)
+        }
+        // console.log(this.selectedCategories)
+      },
+
+      fetchCategories(){
+        axios.get(`http://127.0.0.1:8000/api/categories`)
+        .then(res=> {
+          console.log(res)
+          const { results } = res.data
+          this.categoriesArr = results
+        })
+        .catch(err=> {
+          console.log(err)
+        })
+      },
+      
 
       fetchRestaurants() {
-        axios.get('http://127.0.0.1:8000/api/restaurants')
+        axios.get(`http://127.0.0.1:8000/api/restaurants/categories/${this.categoryid}`)
         .then(res=> {
           console.log(res)
           const { results } = res.data
@@ -42,9 +81,26 @@ import RestaurantsCard from './RestaurantsCard.vue'
           console.log(err)
         })
       },
+
+      fetchAllRestaurants() {
+        axios.get(`http://127.0.0.1:8000/api/restaurants`)
+        .then(res=> {
+          console.log(res)
+          const { results } = res.data
+          this.restaurants = results.data
+        })
+        .catch(err=> {
+          console.log(err)
+        })
+      },
+
     },
     mounted(){
-      this.fetchRestaurants(1)
+      // this.fetchRestaurants(1)
+      this.fetchAllRestaurants(1)
+      this.fetchCategories()
+
+
     },
   }
 </script>
