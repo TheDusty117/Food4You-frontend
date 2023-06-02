c5f1d1170718aef34f4018cbc25474d7ed984e3c
Blaser993 <corradodepinto.design@gmail.com>
2023-06-02 17:36:41 +0200

    raggruppamento di cibi uguali nel carrello

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,20 +1,15 @@
 <template>
-    <div class="container">
-      <div class="grid">
-        <div v-for="(food, index) in store.Cart" :key="index" class="card">
-          <p>{{ food.name }}</p>
-          <p>{{ food.price }}</p>
-          <button @click="RemoveFoodToCart(index, food)">
-            Delete food
-          </button>
-        </div>
-      </div>
+  <div>
+    <div v-for="(food, index) in store.Cart" :key="index" class="card">
+      <p>{{ food.name }} ({{ food.quantity }} unità)</p>
+      <p>{{ food.price }}</p>
+      <button @click="RemoveFoodToCart(index, food)">Delete food</button>
+      <button @click="RemoveOneFoodFromCart(index, food)">Remove One</button>
     </div>
 
-    <div>
-            <PaymentForm />
-    </div>
-  </template>
+    <PaymentForm />
+  </div>
+</template>
   
  <script>
   import store from '../store'
@@ -34,20 +29,44 @@
       }
     },
     mounted() {
-      this.getLocalFood()
+      this.initializeCart();
+    },
+    created() {
+      this.initializeCart();
     },
     methods: {
       RemoveFoodToCart(index, food) {
         this.store.Cart.splice(index, 1);
         localStorage.setItem('foods', JSON.stringify(this.store.Cart));
       },
-      getLocalFood() {
+      RemoveOneFoodFromCart(index, food) {
+        if (food.quantity > 1) {
+          food.quantity -= 1; // Diminuisce la quantità di 1
+        } else {
+          this.store.Cart.splice(index, 1); // Rimuove completamente l'elemento dal carrello
+        }
+        localStorage.setItem('foods', JSON.stringify(this.store.Cart));
+      },
+      initializeCart() {
         const storedData = localStorage.getItem('foods');
         if (storedData) {
           this.store.Cart = JSON.parse(storedData);
         }
       },
     },
+    computed:{
+      uniqueCart() {
+    const uniqueItems = [];
+    this.store.Cart.forEach((food) => {
+      const existingItem = uniqueItems.find((item) => item.name === food.name);
+      if (!existingItem) {
+        uniqueItems.push(food);
+      }
+    });
+    return uniqueItems;
+  },
+
+    }
   }
   </script>
 
--- src/pages/Restaurants.show.vue
+++ src/pages/Restaurants.show.vue
@@ -2,7 +2,7 @@
   <template v-if="loading === false">
     
     <div class="container py-4">
-      <button><router-link :to="{ name: 'cart' }">Cart {{ this.store.Cart.length }}</router-link></button>
+      <button><router-link :to="{ name: 'cart' }">Cart {{ totalQuantity }}</router-link></button>
 
       <div class="restaurant-card">
         <h1 class="text-3xl font-bold mb-4">{{ restaurant.name }}</h1>
@@ -76,14 +76,30 @@ export default {
         });
     },
     AddFoodToCart(food) {
-      // console.log(food)
-      this.store.Cart.push(food)
-      localStorage.setItem('foods', JSON.stringify(this.store.Cart))
+    const existingFood = this.store.Cart.find((item) => item.name === food.name);
+    if (existingFood) {
+      existingFood.quantity += 1; // Incrementa la quantità se il cibo è già presente nel carrello
+    } else {
+      food.quantity = 1; // Imposta la quantità a 1 se è un nuovo cibo nel carrello
+      this.store.Cart.push(food);
     }
+
+    // Salva il carrello nel localStorage
+    localStorage.setItem('foods', JSON.stringify(this.store.Cart));
+  },
   },
   created() {
     this.fetchRestaurant(this.slug);
 
+  },
+  computed:{
+    totalQuantity() {
+    let quantity = 0;
+    this.store.Cart.forEach((food) => {
+      quantity += food.quantity;
+    });
+    return quantity;
+  },
   },
   beforeRouteUpdate(to, from) {
     const newSlug = to.params.slug;
