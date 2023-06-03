aac25abc62106427f8d1776f3a60ed9e05035eac
stefanodisanza <87936828+stefanodisanza@users.noreply.github.com>
2023-06-03 19:44:15 +0200

    Add some style

--- src/components/PaymentForm.vue
+++ src/components/PaymentForm.vue
@@ -1,38 +1,50 @@
 <template>
-    <div>
-        <!-- Form inputs for card details -->
-        <div class="form-group col-3 mt-5">
-            <input type="text" class="form-control" v-model="cardNumber" placeholder="Numero Carta xxxxxxxxxxxxxxxx"
-                @focus="resetValidation('cardNumber')" />
-            <p v-show="validationErrors.cardNumber" class="text-danger">Invalid card number</p>
-        </div>
+    <div class="container">
+        <div class="card mt-4 mx-auto" style="max-width: 500px;">
+            <div class="card-body">
+                <h5 class="card-title">Inserisci i dati di pagamento</h5>
 
-        <div class="form-group col-3 mt-1">
-            <input type="text" class="form-control" v-model="cardName" placeholder="Intestatario Carta"
-                @focus="resetValidation('cardName')" />
-            <p v-show="validationErrors.cardName" class="text-danger">Invalid cardholder name</p>
-        </div>
+                <!-- Form inputs for card details -->
+                <form @submit.prevent="processPayment">
+                    <div class="form-group mt-2">
+                        <input type="text" class="form-control" v-model="cardNumber"
+                            placeholder="Numero Carta xxxxxxxxxxxxxxxx" @focus="resetValidation('cardNumber')"
+                            :class="{ 'is-invalid': validationErrors.cardNumber }" required />
+                        <div class="invalid-feedback">Numero carta non valido</div>
+                    </div>
 
-        <div class="form-group col-3 mt-1">
-            <input type="text" class="form-control" v-model="cardExpiry" placeholder="Data di scadenza xx/xx"
-                @focus="resetValidation('cardExpiry')" />
-            <p v-show="validationErrors.cardExpiry" class="text-danger">Invalid expiry date</p>
-        </div>
+                    <div class="form-group mt-2">
+                        <input type="text" class="form-control" v-model="cardName" placeholder="Intestatario Carta"
+                            @focus="resetValidation('cardName')" :class="{ 'is-invalid': validationErrors.cardName }"
+                            required />
+                        <div class="invalid-feedback">Nome intestatario non valido</div>
+                    </div>
 
-        <div class="form-group col-3 mt-1">
-            <input type="text" class="form-control" v-model="cardCVC" placeholder="CVC"
-                @focus="resetValidation('cardCVC')" />
-            <p v-show="validationErrors.cardCVC" class="text-danger">Invalid CVC</p>
-        </div>
+                    <div class="form-group mt-2">
+                        <input type="text" class="form-control" v-model="cardExpiry" placeholder="Data di scadenza xx/xx"
+                            @focus="resetValidation('cardExpiry')" :class="{ 'is-invalid': validationErrors.cardExpiry }"
+                            required />
+                        <div class="invalid-feedback">Data di scadenza non valida</div>
+                    </div>
 
-        <!-- Submit button -->
-        <button class="btn btn-primary my-4" @click="processPayment">Paga Ora</button>
+                    <div class="form-group mt-2">
+                        <input type="text" class="form-control" v-model="cardCVC" placeholder="CVC"
+                            @focus="resetValidation('cardCVC')" :class="{ 'is-invalid': validationErrors.cardCVC }"
+                            required />
+                        <div class="invalid-feedback">CVC non valido</div>
+                    </div>
+
+                    <!-- Submit button -->
+                    <button type="submit" class="btn btn-primary mt-3">Paga Ora</button>
+                </form>
+            </div>
+        </div>
 
         <!-- Payment status message -->
-        <p v-if="paymentStatus === 'completed'" class="text-success">Pagamento completato</p>
+        <p v-if="paymentStatus === 'completed'" class="text-success mt-3">Pagamento completato</p>
 
         <!-- Go to order summary button -->
-        <button v-if="paymentStatus === 'completed'" class="btn btn-primary" @click="goToOrderSummary">Vai al riepilogo
+        <button v-if="paymentStatus === 'completed'" class="btn btn-primary mt-3" @click="goToOrderSummary">Vai al riepilogo
             dell'ordine</button>
     </div>
 </template>
@@ -56,17 +68,17 @@ export default {
     },
     methods: {
         processPayment() {
-            // Validate card details
-            if (this.validateCardDetails()) {
-                // All card details are valid
+            // Check if the form is valid
+            if (this.validateForm()) {
+                // All inputs are valid
                 this.paymentStatus = 'completed';
             } else {
-                // Invalid card details
+                // Invalid inputs
                 this.paymentStatus = 'invalid';
             }
         },
-        validateCardDetails() {
-            // Reset validation errors
+        validateForm() {
+            let isValid = true;
             this.validationErrors = {
                 cardNumber: false,
                 cardName: false,
@@ -74,51 +86,50 @@ export default {
                 cardCVC: false
             };
 
-            // Implement card details validation logic here
-            // Set validationErrors to true for invalid fields
-
-            // Example card number validation: Check if it contains exactly 16 digits
             if (this.cardNumber.length !== 16 || !/^\d+$/.test(this.cardNumber)) {
                 this.validationErrors.cardNumber = true;
+                isValid = false;
             }
 
-            // Example cardholder name validation: Check if it contains at least 3 characters
             if (this.cardName.length < 3) {
                 this.validationErrors.cardName = true;
+                isValid = false;
             }
 
-            // Example expiry date validation: Check if it is in MM/YY format
             if (!/^(0[1-9]|1[0-2])\/\d{2}$/.test(this.cardExpiry)) {
                 this.validationErrors.cardExpiry = true;
+                isValid = false;
             }
 
-            // Example CVC validation: Check if it contains exactly 3 digits
             if (this.cardCVC.length !== 3 || !/^\d+$/.test(this.cardCVC)) {
                 this.validationErrors.cardCVC = true;
+                isValid = false;
             }
 
-            // Check if there are any validation errors
-            for (const field in this.validationErrors) {
-                if (this.validationErrors[field]) {
-                    return false; // Validation failed
-                }
-            }
-
-            return true; // Validation passed
+            return isValid;
         },
         goToOrderSummary() {
-            // Redirect to order summary page or perform any desired action
             console.log('Go to order summary');
         },
         resetValidation(field) {
-            // Reset validation error for the specified field
             this.validationErrors[field] = false;
         }
     }
 };
 </script>
   
-<style>
-/* Custom styles */
+<style scoped>
+.container {
+    margin-left: 20px;
+}
+
+.invalid-feedback {
+    display: none;
+}
+
+.invalid-feedback.is-invalid {
+    display: block;
+    color: #dc3545;
+}
 </style>
   
--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,103 +1,103 @@
 <template>
-  <div>
-    <div v-for="(food, index) in store.Cart" :key="index" class="card">
-      <p>{{ food.name }} ({{ food.quantity }} unità)</p>
-      <p>{{ food.price }}</p>
-      <button @click="RemoveFoodToCart(index, food)">Delete food</button>
-      <button @click="RemoveOneFoodFromCart(index, food)">Remove One</button>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card-deck">
+          <div v-for="(food, index) in store.Cart" :key="index" class="card mt-3">
+            <div class="card-body">
+              <h5 class="card-title">{{ food.name }} ({{ food.quantity }} unità)</h5>
+              <p class="card-text">€ {{ food.price }}</p>
+              <button @click="RemoveFoodToCart(index, food)" class="btn btn-danger m-2">Cancella cibo</button>
+              <button @click="RemoveOneFoodFromCart(index, food)" class="btn btn-primary m-2">Rimuovi quantità</button>
+            </div>
+          </div>
+        </div>
+        <div v-if="isCartEmpty" class="alert alert-info mt-3">
+          Il carrello è vuoto.
+        </div>
+        <div class="card mt-3">
+          <div class="card-body">
+            <h5 class="card-title">Totale da pagare</h5>
+            <p class="card-text">€ {{ totalToPay }}</p>
+          </div>
+        </div>
+        <button @click="ClearCart" class="btn btn-secondary mt-3">Svuota il carrello</button>
+        <PaymentForm />
+      </div>
     </div>
-    <div v-if="isCartEmpty" >
-      Il carrello è vuoto.
-    </div>
-    <button @click="ClearCart">Svuota il carrello</button>
-    <PaymentForm />
   </div>
 </template>
-  
- <script>
-  import store from '../store'
 
-    import PaymentForm from '../components/PaymentForm.vue'
-  export default {
+<script>
+import store from '../store'
+import PaymentForm from '../components/PaymentForm.vue'
 
-        
-    components: {
-        PaymentForm
-    },
-
-
-    data() {
-      return {
-        store
-      }
-    },
-    mounted() {
-      this.initializeCart();
-    },
-    created() {
-      this.initializeCart();
-    },
+export default {
+  components: {
+    PaymentForm
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  mounted() {
+    this.initializeCart();
+  },
+  created() {
+    this.initializeCart();
+  },
   methods: {
-      RemoveFoodToCart(index, food) {
-        this.store.Cart.splice(index, 1);
-        localStorage.setItem('foods', JSON.stringify(this.store.Cart));
-      },
-      RemoveOneFoodFromCart(index, food) {
-        if (food.quantity > 1) {
-          food.quantity -= 1; // Diminuisce la quantità di 1
-        } else {
-          this.store.Cart.splice(index, 1); // Rimuove completamente l'elemento dal carrello
-        }
-        localStorage.setItem('foods', JSON.stringify(this.store.Cart));
-      },
-      ClearCart() {
-        this.store.Cart = []; // Rimuove tutti gli elementi dal carrello
-        localStorage.removeItem('foods'); // Rimuove il carrello dal localStorage
-      },
-      initializeCart() {
-        const storedData = localStorage.getItem('foods');
-        if (storedData) {
-          this.store.Cart = JSON.parse(storedData);
-        }
-      },
-  },
-  computed:{
-    isCartEmpty() {
-      //console.log(this.isCartEmpty)
-    return this.store.Cart.length === 0; 
-  },
-    uniqueCart() {
-    const uniqueItems = [];
-    this.store.Cart.forEach((food) => {
-      const existingItem = uniqueItems.find((item) => item.name === food.name);
-      if (!existingItem) {
-        uniqueItems.push(food);
-      }
-    });
-    return uniqueItems;
-  },
-
+    RemoveFoodToCart(index, food) {
+      this.store.Cart.splice(index, 1);
+      localStorage.setItem('foods', JSON.stringify(this.store.Cart));
     },
-    
-  }
-  </script>
+    RemoveOneFoodFromCart(index, food) {
+      if (food.quantity > 1) {
+        food.quantity -= 1;
+      } else {
+        this.store.Cart.splice(index, 1);
+      }
+      localStorage.setItem('foods', JSON.stringify(this.store.Cart));
+    },
+    ClearCart() {
+      this.store.Cart = [];
+      localStorage.removeItem('foods');
+    },
+    initializeCart() {
+      const storedData = localStorage.getItem('foods');
+      if (storedData) {
+        this.store.Cart = JSON.parse(storedData);
+      }
+    },
+  },
+  computed: {
+    isCartEmpty() {
+      return this.store.Cart.length === 0;
+    },
+    uniqueCart() {
+      const uniqueItems = [];
+      this.store.Cart.forEach((food) => {
+        const existingItem = uniqueItems.find((item) => item.name === food.name);
+        if (!existingItem) {
+          uniqueItems.push(food);
+        }
+      });
+      return uniqueItems;
+    },
+    totalToPay() {
+      let total = 0;
+      this.store.Cart.forEach((food) => {
+        total += parseFloat(food.price);
+      });
+      return total.toFixed(2);
+    },
+  },
+}
+</script>
 
 <style lang="scss" scoped>
-.grid {
-    display: grid;
-    grid-template-columns: repeat(3, 3fr);
-    gap: 20px;
-}
-
-img {
-    aspect-ratio: 12/9;
-    height: 200px;
-}
-
 .card {
-    padding: 10px;
-    border: 3px solid;
+  border: 3px solid;
 }
-
-
 </style>
