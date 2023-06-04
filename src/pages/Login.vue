515dd8f57588206bac5bea217d9b4cacde5e3e2f
Blaser993 <corradodepinto.design@gmail.com>
2023-06-04 11:29:52 +0200

    refact style register e login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,13 +1,34 @@
 <template>
-
-        <div class="container">
-         <h1>sono il login</h1>
-            <button @click="searchLogin">Login</button>
-        </div>
-
-    
-</template>
-
+    <section>
+       <div class="container text-center">
+           <div class="row justify-content-around pt-5">
+   
+               <h1 class="title">Gestisci il tuo ristorante</h1>
+   
+   
+               
+               
+   
+               <button class="rounded-4 risto px-4 my-3" @click="searchRegister">
+                   <h2 class="my-auto">
+                       Log in
+                   </h2>
+               </button>
+   
+               <div class="info">
+                   <p> Non hai ancora un account? Allora 
+                       <router-link  :to="{ name: 'register' }"><a href="">registrati</a></router-link>
+                       </p>
+               </div>
+               
+   
+           </div>
+           
+       </div>
+   </section>
+   
+       
+   </template>
 <script>
 
 
