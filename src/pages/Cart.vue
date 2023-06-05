70f67a7a503c70714df0177a5bb0f2a50c5b0384
stefanodisanza <87936828+stefanodisanza@users.noreply.github.com>
2023-06-05 15:12:47 +0200

    PUSHAMI TUTTO

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -3,34 +3,46 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12">
+          <!-- Gruppo di carte rappresentanti i cibi nel carrello -->
           <div class="card-deck">
+            <!-- Itera attraverso gli elementi del carrello -->
             <div v-for="(food, index) in cart" :key="index" class="card mb-3 mt-5"
               :style="{ transitionDelay: `${index * 0.1}s` }">
               <div class="card-body">
                 <div class="card-title">
+                  <!-- Nome e quantità del cibo -->
                   <h4>{{ food.name }}</h4>
                   <h6>Quantità: {{ food.quantity }}</h6>
                 </div>
+                <!-- Prezzo del cibo -->
                 <p class="card-text fw-bold">€ {{ calculatePrice(food) }}</p>
                 <div class="button-group">
+                  <!-- Pulsante per rimuovere il cibo dal carrello -->
                   <button @click="removeFoodFromCart(index)" class="btn btn-danger m-2">Cancella cibo</button>
+                  <!-- Pulsante per rimuovere una quantità del cibo dal carrello -->
                   <button @click="removeOneFoodFromCart(index)" class="btn btn-primary m-2">Rimuovi quantità</button>
                 </div>
               </div>
             </div>
           </div>
+          <!-- Messaggio di avviso quando il carrello è vuoto -->
           <div v-if="isCartEmpty" class="alert alert-info mt-3">
             Il carrello è vuoto.
           </div>
+          <!-- Totale da pagare -->
           <div class="card mt-3">
             <div class="card-body">
               <h5 class="card-title">Totale da pagare</h5>
               <p class="card-text">€ {{ totalToPay }}</p>
             </div>
           </div>
+          <!-- Pulsante per svuotare il carrello -->
           <button @click="clearCart" class="btn btn-secondary m-3">Svuota il carrello</button>
+          <!-- Pulsante per tornare alla pagina dei ristoranti -->
           <button @click="confirmAbandonCart" class="btn btn-secondary mt-5 mb-5">Torna ai ristoranti</button>
+          <!-- Componente per il modulo di pagamento -->
           <PaymentForm />
+          <!-- Componente per la finestra di dialogo di conferma per lo svuotamento del carrello -->
           <ConfirmDialog v-if="showConfirmClearDialog" @confirm="confirmClearCart" @cancel="cancelClearCart">
             Sei sicuro di voler svuotare il carrello?
           </ConfirmDialog>
@@ -51,76 +63,85 @@ export default {
   },
   data() {
     return {
-      cart: [],
-      showConfirmClearDialog: false,
+      cart: [], // Array che contiene i cibi nel carrello
+      showConfirmClearDialog: false, // Flag per mostrare la finestra di dialogo di conferma per lo svuotamento del carrello
     }
   },
   mounted() {
-    this.initializeCart();
+    this.initializeCart(); // Inizializza il carrello caricando i dati dal localStorage
   },
   methods: {
     removeFoodFromCart(index) {
       const food = this.cart[index];
       food.removing = true;
       setTimeout(() => {
+        // Rimuove il cibo dal carrello
         this.cart.splice(index, 1);
+        // Salva i dati aggiornati nel localStorage
         localStorage.setItem('foods', JSON.stringify(this.cart));
       }, 300);
     },
     removeOneFoodFromCart(index) {
       const food = this.cart[index];
       if (food.quantity > 1) {
+        // Rimuove una quantità del cibo dal carrello
         food.quantity -= 1;
       } else {
         food.removing = true;
         setTimeout(() => {
+          // Rimuove il cibo dal carrello
           this.cart.splice(index, 1);
+          // Salva i dati aggiornati nel localStorage
           localStorage.setItem('foods', JSON.stringify(this.cart));
         }, 300);
       }
+      // Salva i dati aggiornati nel localStorage
       localStorage.setItem('foods', JSON.stringify(this.cart));
     },
     clearCart() {
-      this.showConfirmClearDialog = true;
+      this.showConfirmClearDialog = true; // Mostra la finestra di dialogo di conferma per lo svuotamento del carrello
     },
     confirmClearCart() {
-      this.cart = [];
-      localStorage.removeItem('foods');
-      this.showConfirmClearDialog = false;
+      this.cart = []; // Svuota il carrello
+      localStorage.removeItem('foods'); // Rimuove i dati dal localStorage
+      this.showConfirmClearDialog = false; // Nasconde la finestra di dialogo di conferma
     },
     cancelClearCart() {
-      this.showConfirmClearDialog = false;
+      this.showConfirmClearDialog = false; // Nasconde la finestra di dialogo di conferma
     },
     initializeCart() {
       const storedData = localStorage.getItem('foods');
       if (storedData) {
-        this.cart = JSON.parse(storedData);
+        this.cart = JSON.parse(storedData); // Carica i dati dal localStorage nel carrello
       }
     },
     calculateTotalPrice() {
       let total = 0;
       this.cart.forEach((food) => {
+        // Calcola il prezzo totale sommando il prezzo di ogni cibo moltiplicato per la quantità
         total += parseFloat(food.price) * food.quantity;
       });
       return total.toFixed(2);
     },
     calculatePrice(food) {
       if (food.quantity > 1) {
+        // Calcola il prezzo del cibo moltiplicando il prezzo per la quantità se la quantità è maggiore di 1
         return (parseFloat(food.price) * food.quantity).toFixed(2);
       } else {
         return food.price;
       }
     },
     confirmAbandonCart() {
+      // Reindirizza l'utente alla pagina dei ristoranti
       this.$router.push('/restaurants');
     },
   },
   computed: {
     isCartEmpty() {
-      return this.cart.length === 0;
+      return this.cart.length === 0; // Verifica se il carrello è vuoto
     },
     totalToPay() {
-      return this.calculateTotalPrice();
+      return this.calculateTotalPrice(); // Calcola il totale da pagare
     },
   },
 }
